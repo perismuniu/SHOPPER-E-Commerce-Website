ffc3a378f8ff5e629f699d58555f8b1b27916b4a
Peris Muniu <122822923+perismuniu@users.noreply.github.com>
2024-08-21 18:38:21 +0300

    Admin file set up.

---
+++ Admin/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        Pacifico: ["Pacifico", "cursive"],
+        Outfit: ["Outfit", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
